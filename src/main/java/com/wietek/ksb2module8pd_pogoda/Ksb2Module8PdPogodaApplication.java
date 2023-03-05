cedd3ab3a4154bd04b54acad1e6ba6275e1ee6ac
PLRADMWI <mateusz180895@wp.pl>
2023-03-05 14:31:19 +0100

    first commit

---
+++ src/main/java/com/wietek/ksb2module8pd_pogoda/Ksb2Module8PdPogodaApplication.java
@@ -0,0 +1,13 @@
+package com.wietek.ksb2module8pd_pogoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksb2Module8PdPogodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ksb2Module8PdPogodaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wietek/ksb2module8pd_pogoda/Ksb2Module8PdPogodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wietek.ksb2module8pd_pogoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksb2Module8PdPogodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
